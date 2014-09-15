db3df4fff1bce39df1b50ff72a4f82c5ff55b675
Chris Card <ccard@mymail.mines.edu>
2014-09-15 09:46:17 -0600

    make file working with main file and add main file to src

--- Makefile
+++ Makefile
@@ -3,14 +3,17 @@ HEAD = ./head
 CXXFLAGS += -I$(HEAD)
 EXEC = parseSkeleton
 BIN = ./bin
-OBJECTS = $(BIN)/main.o $(BIN)/p.o
+SRC = ./src
+OBJECTS = $(BIN)/main.o
 
 all: $(EXEC)
 
 $(EXEC): $(OBJECTS)
 	$(GCC) $(CXXFLAGS) $(OBJECTS) -o $(EXEC)
 
-$(BIN)/main.o: main.cpp $(BIN)/other
+$(BIN)/main.o: $(SRC)/main.cpp
+	$(GCC) -c $(CXXFLAGS) $^ -o $@
 
 clean:
 	rm -rf $(BIN)/*.o
+	rm -rf $(EXEC)
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * Author: Chris Card
+ * 
+ * runns the program for all test and trianing set
+ * building 
+ */
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+bool process_file(string file){
+	return false;
+}
+
+bool process_args(int argc, char const *argv[], string &file){
+	if (argc != 2) {
+		cerr << "Usage:" << endl;
+		cerr << "parseSkeleton <data_file>" << endl;
+		cerr << "<data_file> : must be in the expected format"<< endl;
+		return false;
+	} else {
+		file = string(argv[1]);
+		return true;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	string file;
+	if (!process_args(argc,argv,file)) return 2;
+	process_file(file);
+	return 0;
+}

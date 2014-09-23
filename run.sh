ec5f5cc36782a80b07cf3e6ac963045b1df86930
Chris Card <ccard@mymail.mines.edu>
2014-09-23 14:02:18 -0600

    run file is now working

--- run.sh
+++ run.sh
@@ -4,6 +4,13 @@ rad="RAD"
 hjdp="HJDP"
 hod="HOD"
 
+radfile="rad"
+radtfile="rad.t"
+hjdpfile="hjdp"
+hjdptfile="hjdp.t"
+hodfile="hod"
+hodtfile="hod.t"
+
 exe="parseSkeleton"
 
 function usage {
@@ -14,24 +21,73 @@ function usage {
 	echo "           ${hod}"
 }
 
+function run {
+	local message=$1
+	local stmt=$2
+
+	echo -n "${message} .... "
+	local res=`eval $stmt`
+	echo "done"
+	echo "Output:"
+	echo $res
+}
+
 if [[ $# -ne 1 ]]; then
 	usage
 else
 	if [[ $1 != $rad && $1 != $hjdp && $1 != $hod ]]; then
 		usage
 	else
-		echo -n "Creating training data .... "
-		train="./${exe} -tr ${1} \"./data/dataset/Train/\" human_actions.txt"
-		res_train=`eval $train`
-		echo "done"
-		echo "Output:"
-		echo $res_train
+		trfile=""
+		tefile=""
+		if [[ $1 == $rad ]]; then
+			trfile=$radfile
+			tefile=$radtfile
+		elif [[ $1 == $hjdp ]]; then
+			trfile=$hjdpfile
+			tefile=$hjdptfile
+		elif [[ $1 == $hod ]]; then
+			trfile=$hodfile
+			tefile=$hodtfile
+		fi
 
-		echo -n "Creating testing data .... "
+		#scaled training files
+		trscale="${trfile}.scale"
+		tescale="${tefile}.scale"
+		modelfile="${trscale}.model"
+		predfile="${tefile}.predict"
+
+		#commands for svm
+		scale="svm-scale"
+		pred="svm-predict"
+		trsvm="svm-train"
+
+		#flags for svm
+		argscale="-l -1 -u 1 -s range1"
+		argscalet="-r range1"
+		argtrain="-c 2 -g 2"
+
+		#execution commands for svm
+		exscale="${scale} ${argscale} ${trfile} > ${trscale}"
+		exscalet="${scale} ${argscalet} ${tefile} > ${tescale}"
+		extrain="${trsvm} ${argtrain} ${trscale}"
+		expred="${pred} ${tescale} ${modelfile} ${predfile}"
+
+		#execution commands for my program to create the skeletons data
+		train="./${exe} -tr ${1} \"./data/dataset/Train/\" human_actions.txt"
 		test_p="./${exe} -te ${1} \"./data/dataset/Test/\" test_human_actions.txt"
-		res_test=`eval $test_p`
-		echo "done"
-		echo "Output:"
-		echo $res_test
+
+		#Execution of my program and libsvm
+		run "Creating training data" "${train}" 
+
+		run "Creating testing data" "${test_p}"
+
+		run "Scaling traing data" "${exscale}"
+
+		run "Scaling testing data" "${exscalet}"
+
+		run "Training svm" "${extrain}"
+
+		run "Running prediction" "${expred}"
 	fi
 fi
